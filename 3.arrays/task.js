8750948fbc375dbc9c20a48f872ddd1f55c00669
Dima0302F <ugo0302@gmail.com>
2026-05-31 11:21:24 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+    // Длины массивов разные, сразу возвращаем false
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    
+    // Сравнения элементов на одинаковых индексах
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    // Фильтруем по полу
+    const filteredUsers = users.filter(user => user.gender === gender);
+    
+    // Если нет подходящих пользователей, возвращаем 0
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+    
+    // Суммируем возраста и вычисляем среднее
+    const sumAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    
+    return sumAge / filteredUsers.length;
 }
